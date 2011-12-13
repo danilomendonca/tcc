02e0a6e2bc6b39557e755fba10378aed8aa07c18
Danilo Mendonca <danilofilgueira@ig.com.br>
2011-12-12 22:07:14 -0200

    Arquivos

---
+++ exec.bat
@@ -0,0 +1,10 @@
+latex relatorio
+
+bibtex relatorio
+makeindex relatorio.nlo -s nomencl.ist -o relatorio.nls
+latex relatorio
+
+latex relatorio
+
+dvips -t a4 -o relatorio.ps tese.dvi
+ps2pdf relatorio.ps relatorio.pdf
